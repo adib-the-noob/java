a59a48a1b9bc71fd1477c63e02e817fe0e4234c3
adib-the-noob <mohammed.adib1104@gmail.com>
2025-07-07 20:32:59 +0600

    all datatypes added

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,23 @@ public class Main {
         int x = 12;
         double y = 12.23;
 
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println("Hello World!");
+        System.out.println("The Price is " + x);
+        System.out.println("The Price 1 is " + y);
+        System.out.println("=====================");
+
+        double price = 12.21;
+        char grade = 'A';
+        char currency = '$';
+
+        System.out.println(grade);
+        System.out.println(currency);
+        System.out.println("=====================");
+
+        boolean isStudent = true;
+        if (isStudent) {
+            System.out.println("Student is true");
+        } else {
+            System.out.println("Student is false");
+        }
     }
 }
