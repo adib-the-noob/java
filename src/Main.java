d8d8a79bb31b8c453272ebd5381e04dc62c7b63a
adib-the-noob <mohammed.adib1104@gmail.com>
2025-07-07 20:06:33 +0600

    all code added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 12;
+        double y = 12.23;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println("Hello World!");
+    }
+}
